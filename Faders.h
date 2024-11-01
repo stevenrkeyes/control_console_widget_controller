8b02bef9662b9151f0abbc919598d450027348d5
Rohit Mukherjee <djbhindi@gmail.com>
2024-10-31 22:53:12 -0700

    add faders module

---
+++ Faders.h
@@ -0,0 +1,39 @@
+#ifndef FADERS_H
+#define FADERS_H
+
+#include "ConsoleWidget.h"
+
+// TODO: Decide if we're going to use:
+//    - 1 channel per module
+//    - 1 channel per microcontroller
+//    - 1 channel globally (defaulting to this for simplicity)
+constexpr int FADERS_MIDI_CHANNEL = 1;
+constexpr int FADER_COUNT = 8;
+
+// Uses pings 14 - 21.
+constexpr int FADER_PIN_OFFSET = 14;
+
+// These may be used elsewhere; define in a more general location?
+constexpr int MAX_MIDI_VELOCITY = 127;
+constexpr int MIN_MIDI_VELOCITY = 1;
+
+constexpr int MAX_FADER_VALUE = 1023;
+constexpr int MIN_FADER_VALUE = 0;
+
+class Faders : public ConsoleWidget {
+public:
+  Faders();
+  void setup() override;
+  void CheckDataSendMIDI() override;
+  void UpdateAnimationFrame() override;
+
+private:
+  // TODO: Refactor this to a struct?
+  int fader_pins[FADER_COUNT] = { 14, 15, 16, 17, 18, 19, 20, 21 };
+  bool fader_changed[FADER_COUNT] = { false, false, false, false, false, false, false, false };
+  int fader_values[FADER_COUNT] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+  // C Major scale.
+  int fader_notes[FADER_COUNT] = { 48, 50, 52, 53, 55, 57, 59, 60 };
+};
+
+#endif
--- midi_widget_controller.ino
+++ midi_widget_controller.ino
@@ -1,16 +1,19 @@
 #include "ConsoleWidget.h"
 #include "NeatButton.h"
 #include "PowerButtons.h"
+#include "Faders.h"
 
 // Add console widgets here
 // NeatButton is just an example widget that uses the built-in LED and a single button on pin 5
 NeatButton neatButton;
 PowerButtons powerButtons;
-ConsoleWidget* widgets[] = { &neatButton, &powerButtons };
+Faders faders;
+ConsoleWidget* widgets[] = { &neatButton, &powerButtons, &faders };
 
 void setup() {
   for (auto widget : widgets) {
     widget->setup();
+    
   }
 }
 
